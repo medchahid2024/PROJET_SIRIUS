b3f819c507f7aac977384d0518f52e01d79ee9f0
medchahid2024 <medchahid2022@gmail.com>
2025-03-13 00:43:19 +0100

    conflit

--- xmart-frontend/src/main/java/edu/ezip/ing1/pds/ControlStage.java
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/ControlStage.java
@@ -94,18 +94,18 @@ public class ControlStage {
     }
 
     public void postuler(ActionEvent actionEvent) throws IOException, InterruptedException {
-        Stagee stageSelectionne = stageList.get(currentIndex); // Récupérer le stage affiché
-        int idOffre = stageSelectionne.getId();
-        Candidater candidater= new Candidater();
-        candidater.setId(idOffre);
+//        Stagee stageSelectionne = stageList.get(currentIndex); // Récupérer le stage affiché
+//        int idOffre = stageSelectionne.getId();
+//        Candidater candidater= new Candidater();
+//        candidater.setId(idOffre);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Candidater.fxml"));
         Stage stage = new Stage();
 
         Scene offre = new Scene(fxmlLoader.load(), 700, 700);
         stage.setScene(offre);
         stage.setTitle("Candidature");
-        Candidater candidatureController = fxmlLoader.getController();
-        candidatureController.setId(idOffre);
+//        Candidater candidatureController = fxmlLoader.getController();
+//        candidatureController.setId(idOffre);
 
         stage.show();
     }
