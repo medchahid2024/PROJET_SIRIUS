a4baf2a01bcfb21e75b3d28520f9073a7731620b
medchahid2024 <medchahid2022@gmail.com>
2025-04-02 00:00:12 +0200

    script bash

--- Deployer.sh
+++ Deployer.sh
@@ -7,9 +7,11 @@ PWD="server"
 PATH_BACK_VM="/home/server/xmart-zity-backend.jar"
 PATH_BACK_ABDEL="/mnt/c/Users/56/Desktop/WorkSpace2/PROJET_SIRIUS-main/xmart-city-backend/target/xmart-zity-backend-1.0-SNAPSHOT-jar-with-dependencies.jar"
 PATH_BACK_HOSSAME="/mnt/c/Users/arsal/OneDrive/Bureau/workspace/prototype-ing1/xmart-city-backend/target/xmart-zity-backend-1.0-SNAPSHOT-jar-with-dependencies.jar"
-
+PATH_BACK_MOHAMMED="/mnt/c/Users/OMEN/Desktop/prototype-ing1/xmart-city-backend/target/xmart-zity-backend-1.0-SNAPSHOT-jar-with-dependencies.jar"
 # Transfert du fichier vers le serveur
-scp "$PATH_BACK_ABDEL" "$USER@$IP:$PATH_BACK_VM"
+#scp "$PATH_BACK_ABDEL" "$USER@$IP:$PATH_BACK_VM"
+scp "$PATH_BACK_MOHAMMED" "$USER@$IP:$PATH_BACK_VM"
+
 
 # Exécution du fichier sur le serveur
 sshpass -p "$PWD" ssh "$USER@$IP" "java -jar $PATH_BACK_VM"
--- ScriptFront.bat
+++ ScriptFront.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Vérification de l'existence du fichier JAR...
-cd /d "C:\Users\56\Desktop\WorkSpace2\PROJET_SIRIUS-main\xmart-frontend\target"
+cd /d "C:\Users\OMEN\Desktop\prototype-ing1\xmart-frontend\target"
 
-java --module-path "C:\Users\56\Desktop\JavaFx\javafx-sdk-23.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar xmart-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar
+java --module-path "C:\Users\OMEN\Desktop\JavaFx\javafx-sdk-23.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar xmart-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar
 w
